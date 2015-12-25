8f1e761bfc9c7fa7962aaa01f799da26f504191c
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2015-12-26 04:42:02 +0900

    [add] meu-corpus

--- test.rb
+++ test.rb
@@ -1,8 +1,10 @@
 # encoding: utf-8
 require './change-to-meu-with-natto.rb'
+require './meu-corpus.rb'
 
 puts "文章を入力するめう"
-#text = gets()
-text = "めうとにゃんにゃんする"
-puts change_to_meu_with_natto(text)
+text = gets()
+text = change_to_meu_with_natto(text)
+text = changeToMeu(text)
 
+puts text
